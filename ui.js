00a3c3172a7f812a2d0123b9ad87a60cd1045107
Benjamin Robson <robsonhbenjamin@gmail.com>
2024-07-09 11:21:22 +0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,6 +1,5 @@
 const blocksContainer = document.querySelector(".blocks");
 
-/*
 const palette = [
   "#FCDC94",
   "#C8CFA0",
@@ -10,18 +9,7 @@ const palette = [
   "#296381",
   "#184769",
 ].reverse();
-*/
 
-const palette = [
-  "#FCDC94", // Light and bright contrast to dark colors
-  "#C8CFA0", // Light and bright contrast to dark colors
-  "#78ABA8", // Light and bright contrast to dark colors
-  "#4D9DB4", // Light and bright contrast to dark colors
-  "#3A7F9C", // Light and bright contrast to dark colors
-  "#296381", // Light and bright contrast to dark colors
-  "#184769", // Light and bright contrast to dark colors
-];
-/*
 const backgroundPalette = [
   "#FFFFF0",
   "#E6E6FA",
@@ -31,19 +19,6 @@ const backgroundPalette = [
   "#F5F5DC",
   "#FDF5E6",
   "#FFF8DC",
-];
-*/
-
-
-const backgroundPalette = [
-  "#2F4F4F", // Dark Slate Grey
-  "#4B0082", // Indigo
-  "#006400", // Dark Green
-  "#8B4513", // Saddle Brown
-  "#8B0000", // Dark Red
-  "#556B2F", // Dark Olive Green
-  "#8B4513", // Dark Orange
-  "#8B008B", // Dark Magenta
 ];
 
 let currentIndex = 0;
